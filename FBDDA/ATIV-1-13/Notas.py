31b2cfe60a495088405bb241e10c27e47a270bb1
MariMari021 <133662908+MariMari021@users.noreply.github.com>
2024-03-09 17:58:01 -0300

    Add files via upload

---
+++ FBDDA/ATIV-1-13/Notas.py
@@ -0,0 +1,51 @@
+'''
+
+#Calculadora
+print(1 + 1)
+print(1 - 10)
+print(5 + 3)
+
+#String / Texto
+print('Meu nome é Mariana')
+
+
+
+# Entrada de Dados
+N1 = int(input('Digite a sua idade: '))
+Idade_Nova = N1 + 1000000
+print(f'Sua idade é {Idade_Nova} ')
+'''
+
+#Strings
+'''
+Nome = 'Mariana Gomes'
+print(Nome[0])
+print(Nome[0:4])
+print(Nome[::2])
+
+tamanho = len(Nome)
+print(len(Nome))
+print(f'A quantidade de l é {Nome.count("i")}')
+mai = Nome.upper()
+min = Nome.lower()
+
+print(f'{mai} e {min}')
+
+Name = input('Digite seu nome: ').strip()
+
+print(Name)
+
+
+
+for contador in range (1, 10):
+    print('*'),
+
+for contador in range (1, 10):
+    print(contador, end='')
+'''
+
+soma = 0
+for i in range(1, 11):
+    idade = int(input('Digite sua idade: '))
+    soma= soma + idade
+    print(soma)
